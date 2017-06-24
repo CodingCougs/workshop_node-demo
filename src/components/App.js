a219b27828b2694afe1f07b03c02e7fa0ec1f03e
10tranr <10tranr@gmail.com>
2017-06-24 00:36:15 -0700

    Added tips via comments

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 
 // Import other components here
 
+// ----------------------------
+
+
 export default class App extends Component {
     render() {
+        // Insert JSX to be rendered to localhost here
         return (
         <div>
+            Hello!
         </div>
         );
     }
